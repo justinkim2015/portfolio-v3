54a877bf1af069efa8825d6a681a37739c8c1fe7
Justin Kim <justinkim2015@gmail.com>
2023-01-27 22:53:12 +0900

    Add working hamburger menu

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -3,16 +3,21 @@ import hamburger from './white-hamburger.png'
 import jk from './jk.png'
 
 const Navbar = () => {
+  const toggle = () => {
+    let hamburger = document.querySelector('.hamburger-menu')
+    hamburger.classList.toggle('hidden')
+  }
+
   return (
     <nav id='navbar'>
       <img src={jk} className='logo'></img>
-      <img src={hamburger} className='hamburger-button'></img>
+      <img src={hamburger} className='hamburger-button' onClick={toggle}></img>
 
-      <ul className='hamburger-menu'>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Contact</li>
+      <ul className='hamburger-menu hidden'>
+        <li className='link'>About</li>
+        <li className='link'> Skills</li>
+        <li className='link'>Projects</li>
+        <li className='link'>Contact</li>
       </ul>
     </nav>
   )
