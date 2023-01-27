02ab27e841defae11b6267aaf567be3695249ed9
Justin Kim <justinkim2015@gmail.com>
2023-01-27 23:08:22 +0900

    create basic home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './navbar/Navbar.js'
+import Home from './home/Home.js'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ src/home/Home.js
@@ -0,0 +1,16 @@
+import './home.css'
+import sunrise from './sunrise-desat.png'
+
+const Home = () => {
+  return (
+    <section id='home'>
+      <div id='banner-content'>
+        <h2 className='hello'>Hi there,</h2>
+        <p className='name'>I'm Justin Kim.</p>
+        <p className='my-desc'>Full-stack web developer</p>
+      </div>
+    </section>
+  )
+}
+
+export default Home
