5184428e72c4edb495311af675ad151b60400bbc
Justin Kim <justinkim2015@gmail.com>
2023-01-28 20:27:35 +0900

    Create header for projects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './navbar/Navbar.js'
 import Home from './home/Home.js'
 import About from './about/About.js'
 import Skills from './skills/Skills.js'
+import Projects from './projects/Projects.js'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Home />
         <About />
         <Skills />
+        <Projects />
       </div>
     </div>
   );
---
+++ src/projects/Projects.js
@@ -0,0 +1,15 @@
+import './projects.css'
+
+const Projects = () => {
+  return (
+    <section id='projects'>
+      <div className="projects-header">
+        <h2 className='projects-title'>My Projects</h2>
+        <h6 className='projects-subtitle'>Check out some the projects I've made.</h6>
+        <div className='underbar'></div>
+      </div>
+    </section>
+  )
+}
+
+export default Projects
