ca30bf77d215af3a75f0e3942f9288e48a3743e6
Justin Kim <justinkim2015@gmail.com>
2023-01-28 20:14:34 +0900

    Add skill logos

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,13 +1,19 @@
 import './skills.css'
+import front from './front.png'
+import back from './back.png'
+import tool from './tool.png'
 
 const Skills = () => {
   return (
     <section id='skills'>
-      <h2 className='skills-title'>Skills</h2>
-      <h6 className='skills-subtitle'>My core competencies and things I do.</h6>
-      <div className='underbar'></div>
+      <div class="skills-header">
+        <h2 className='skills-title'>Skills</h2>
+        <h6 className='skills-subtitle'>My core competencies and things I do.</h6>
+        <div className='underbar'></div>
+      </div>
 
       <div className='frontend'>
+        <div className='circle'><img src={front} className='skill-icon'></img></div>
         <h3 className='skill-title'>Front-end Development</h3>
         <ul className='skill-list'>
           <li className='skill'>JavaScript</li>
@@ -18,6 +24,7 @@ const Skills = () => {
       </div>
 
       <div className='backend'>
+        <div className='circle'><img src={back} className='skill-icon'></img></div>
         <h3 className='skill-title'>Back-end Development</h3>
         <ul className='skill-list'> 
           <li className='skill'>Ruby & Ruby on Rails</li>
@@ -29,6 +36,7 @@ const Skills = () => {
       </div>
 
       <div className='dev'>
+        <div className='circle'><img src={tool} className='skill-icon'></img></div>
         <h3 className='skill-title'>Development Tools</h3>
         <ul className='skill-list'>
           <li className='skill'>Git & Github</li>
