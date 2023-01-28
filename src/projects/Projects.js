5fe7ab05ec5f53918e8e735b74693028afa29d1b
Justin Kim <justinkim2015@gmail.com>
2023-01-28 20:55:09 +0900

    Add images to project section

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,4 +1,8 @@
 import './projects.css'
+import karaoke from './karaoke.png'
+import railsbook from './railsbook.png'
+import chess from './chess.png'
+import weather from './weather.png'
 
 const Projects = () => {
   return (
@@ -7,6 +11,22 @@ const Projects = () => {
         <h2 className='projects-title'>My Projects</h2>
         <h6 className='projects-subtitle'>Check out some the projects I've made.</h6>
         <div className='underbar'></div>
+      </div>
+
+      <div className='project'>
+        <img src={karaoke} className='project-image'></img>
+      </div>
+
+      <div className='project'>
+        <img src={railsbook} className='project-image'></img>
+      </div>
+
+      <div className='project'>
+        <img src={chess} className='project-image'></img>
+      </div>
+
+      <div className='project'>
+        <img src={weather} className='project-image'></img>
       </div>
     </section>
   )
