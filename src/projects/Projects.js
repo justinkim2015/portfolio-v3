e13254766e005c0fcc549dc965c5b3ecf7663c68
Justin Kim <justinkim2015@gmail.com>
2023-01-30 14:51:00 +0900

    Add text to projects

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -14,26 +14,41 @@ const Projects = () => {
       </div>
 
       <div className='project'>
-        <h3>Karaoke Song Organizer</h3>
         <img src={karaoke} className='project-image'></img>
+        <div class="project-text">
+          <h3 className='project-name'>Karaoke Song Organizer</h3>
+          <p className='project-desc'>This Ruby on Rails application keeps track of karaoke songs and stores. By using several Google APIs, Genius API, an IP geocoding API, and webscraping it is able to find and save song lyrics and nearby karaoke stores.</p>
+        </div>
       </div>
 
       <div className='underbar'></div>
 
       <div className='project'>
         <img src={railsbook} className='project-image'></img>
+        <div class="project-text">
+          <h3 className='project-name'>RailsBook</h3>
+          <p className='project-desc'>I made this with Ruby on Rails and hosted on Render. This is a social media site where one can post, add friends, and upload photos.</p>
+        </div>
       </div>
 
       <div className='underbar'></div>
 
       <div className='project'>
         <img src={chess} className='project-image'></img>
+        <div class="project-text">
+          <h3 className='project-name'>Two-Player CLI Chess</h3>
+          <p className='project-desc'>This is a two-player chess game built in Ruby using OOP principles.  Players can save the game and continue from any point.</p>
+        </div>
       </div>
 
       <div className='underbar'></div>
 
       <div className='project'>
         <img src={weather} className='project-image'></img>
+        <div class="project-text">
+          <h3 className='project-name'>iPad Weather App</h3>
+          <p className='project-desc'>This is a single-page weather application made with ReactJS and vanilla CSS. By leveraging the data of a Geolocation API and a weather API this app shows you real-time data of the weather in your area.</p>
+        </div>
       </div>
 
     </section>
