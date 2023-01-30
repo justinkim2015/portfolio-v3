1cff9305922416abd43688dd4a7e38089749f52d
Justin Kim <justinkim2015@gmail.com>
2023-01-30 15:10:15 +0900

    Create contact section

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -2,7 +2,17 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <div>hi</div>
+    <section id='contact'>
+      <div className='contact-info'>
+        <h3 className='contact-title'>Contact Info</h3>
+        <p className='contact-details'>Location:  Suginami, Tokyo</p>
+        <p className='contact-details'>Email:  justinjkim2015@gmail.com</p>
+        <p className='contact-details'>LinkedIn:  https://www.linkedin.com/</p>
+      </div>
+      <div className='contact-form'>
+
+      </div>
+    </section>
   )
 };
 
