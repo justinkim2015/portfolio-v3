8ce72efda38a7ae27ba455c43476e536dd4058e9
Justin Kim <justinkim2015@gmail.com>
2023-02-01 20:59:04 +0900

    Add link to logo

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <nav id='navbar'>
       <div className='mobile'>
-        <img src={jk} className='logo'></img>
+        <a href="#"><img src={jk} className='logo'></img></a>
         <img src={hamburger} className='hamburger-button' onClick={toggle}></img>
         <ul id='menu' className='hamburger-menu-inactive hamburger-menu'>
           <a href="#about" className='link'>About</a>
